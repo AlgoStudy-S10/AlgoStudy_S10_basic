634913841171cf966bc88531a61a25ee2a9184dc
SeMin Kim <83855438+tpals0409@users.noreply.github.com>
2026-02-02 10:24:10 +0900

    [BOJ] 9999 워크플로우테스트

--- workspace/김세민/template/Main.java
+++ workspace/김세민/template/Main.java
@@ -1,7 +1,12 @@
 package ksm.template;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args){
-   
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("워크플로우 테스트 문제 풀이: " + n);
+        sc.close();
     }
 }
