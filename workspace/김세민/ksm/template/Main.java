121dec7abb5c9b64d0837f898e1e85e4b8dc32d1
SeMin Kim <83855438+tpals0409@users.noreply.github.com>
2026-02-04 17:16:02 +0900

    [BOJ] 0639

--- workspace/김세민/ksm/template/Main.java
+++ workspace/김세민/ksm/template/Main.java
@@ -4,65 +4,68 @@ import java.io.*;
 
 
 public class Main {
-    public static class Line{
-        int weight;
-        int next;
-        Line(int weight, int next){
-            this.weight = weight;
-            this.next = next;
+    public static class Node {
+        int value;
+        Node left;
+        Node right;
+        Node parent;
+
+        Node() {
         }
+
+        public Node(int value) {
+            super();
+            this.value = value;
+            this.left = null;
+            this.right = null;
+            this.parent = null;
+        }
+
     }
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
+        // TODO Auto-generated method stub
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = null;
-        ArrayList<Line>[] tree = new ArrayList[N+1];
-        for (int i = 1; i <= N; i++) {
-            tree[i] = new ArrayList<>();
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        String line;
+        while ((line = br.readLine()) != null && !line.isEmpty()) {
+            q.offerLast(Integer.parseInt(line));
         }
-        for(int i=1; i<N; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
-            tree[start].add(new Line(weight, end));
-            tree[end].add(new Line(weight, start));
-        }
-        int answer = 0;
-        int lastNode = bfs(tree, 1)[0];
-        answer = bfs(tree, lastNode)[1];
 
-        System.out.println(answer);
-    }
-
-
-    public static int[] bfs(ArrayList<Line>[] tree, int startNode) {
-        Deque<Integer> q = new ArrayDeque<>();
-        Deque<Integer> w = new ArrayDeque<>();
-
-        q.offer(startNode);
-        w.offer(0);
-        int answer = 0;
-        int lastNode = 1;
-        boolean[] visited = new boolean[tree.length];
-        visited[startNode] = true;
-
-        while(!q.isEmpty()) {
-            int nowNode = q.pollFirst();
-            int nowWeight = w.pollFirst();
-            for(int i=0; i<tree[nowNode].size(); i++) {
-                if(!visited[tree[nowNode].get(i).next]) {
-                    visited[tree[nowNode].get(i).next] = true;
-                    q.offerLast(tree[nowNode].get(i).next);
-                    w.offerLast(nowWeight+tree[nowNode].get(i).weight);
-                    if(answer<nowWeight+tree[nowNode].get(i).weight) {
-                        answer = nowWeight+tree[nowNode].get(i).weight;
-                        lastNode = tree[nowNode].get(i).next;
+        Node start = new Node(q.pollFirst());
+        while (!q.isEmpty()) {
+            int tmp = q.pollFirst();
+            Node now = start;
+            while (true) {
+                if (tmp < now.value) {
+                    if (now.left == null) {
+                        now.left = new Node(tmp);
+                        now.left.parent = now;
+                        break;
+                    } else {
+                        now = now.left;
+                    }
+                } else {
+                    if (now.right == null) {
+                        now.right = new Node(tmp);
+                        now.right.parent = now;
+                        break;
+                    } else {
+                        now = now.right;
                     }
                 }
             }
         }
-        return new int[] {lastNode, answer};
+        printNode(start);
+    }
+
+    public static void printNode(Node start) {
+        if (start.left != null) {
+            printNode(start.left);
+        }
+        if (start.right != null) {
+            printNode(start.right);
+        }
+        System.out.println(start.value);
     }
 }
