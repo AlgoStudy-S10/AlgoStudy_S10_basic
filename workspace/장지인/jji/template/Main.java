cdb6c222c3cdc99bd6e598645c89a45ae05b9553
JIIN <j00j12i11@duksung.ac.kr>
2026-02-24 16:05:29 +0900

    [BOJ] 1987 알파벳

--- workspace/장지인/jji/template/Main.java
+++ workspace/장지인/jji/template/Main.java
@@ -4,68 +4,48 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-	static int N;
-	static char[][] grid;
-	static boolean[][] isVisited;
+	
+	static int R, C, BEST;
+	static char[][] board;
+	static boolean[] alpha;
 	static int[] dr = { -1, 1, 0, 0 };
 	static int[] dc = { 0, 0, -1, 1 };
-
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		N = Integer.parseInt(br.readLine().trim());
-
-		grid = new char[N][];
-		isVisited = new boolean[N][N];
-
-		for (int i = 0; i < N; i++) {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		
+		board = new char[R][C];
+		alpha = new boolean['Z'-'A'+1];
+		BEST = 0;
+		
+		for (int i=0; i<R; i++) {
 			String line = br.readLine();
-			grid[i] = line.toCharArray();
+			board[i]= line.toCharArray();
 		}
-
-		// 적록색약이 아닌 기준 dfs
-		int A = 0;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (!isVisited[i][j]) {
-					isVisited[i][j] = true;
-					dfs(i, j, grid[i][j]);
-					A++;
-				}
-				// 이미 지났을테니 Gray로 변경함
-				if (grid[i][j] != 'B') {
-					grid[i][j] = 'G';
-				}
-			}
-		}
-		// 적록색약 기준 dfs
-		isVisited = new boolean[N][N];
-		int B = 0;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (!isVisited[i][j]) {
-					isVisited[i][j] = true;
-					dfs(i, j, grid[i][j]);
-					B++;
-				}
-			}
-		}
-		System.out.println(A + " " + B);
+		
+		alpha[board[0][0] - 'A'] = true;
+		dfs(0,0,1);
+		
+		System.out.println(BEST);
 	}
-
-	static void dfs(int r, int c, char t) {
-		int nr, nc;
-		for (int i = 0; i < 4; i++) {
-			nr = r + dr[i];
-			nc = c + dc[i];
-
-			if (nr >= 0 && nc >= 0 && nr < N && nc < N && !isVisited[nr][nc]) {
-				if (grid[nr][nc] != t) {
-					continue;
+	static void dfs(int r, int c, int cnt) {
+		BEST = Math.max(BEST, cnt);
+		
+		for (int i=0; i<4; i++) {
+			int nr = r + dr[i];
+			int nc = c + dc[i];
+			// 범위체크
+			if (nr >=0 && nc >=0 && nr < R && nc < C) {
+				int id = board[nr][nc] - 'A';
+				if (!alpha[id]) {
+					alpha[id] = true;
+					dfs(nr, nc, cnt+1);
+					alpha[id] = false;
 				}
-				isVisited[nr][nc] = true;
-				dfs(nr, nc, t);
 			}
 		}
 	}
